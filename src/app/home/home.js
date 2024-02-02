9731f9d36af902835fe19ba0e9ea5df7482e772e
Cody Huang <shaowei.huang@verizonmedia.com>
2024-02-02 16:35:31 +0800

    Remove unnecessary items

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -17,13 +17,4 @@ angular.module( 'ngScorekeeper.home', [
 
 .controller( 'HomeCtrl', function HomeController( $scope , $window) {
     $scope.isonline = $window.navigator.onLine;
-
-    $scope.show_social = function()
-    {
-        if ($window.navigator.onLine)
-        {
-            return 'home/social.tpl.html';
-        }
-        return null;
-    };
 });
